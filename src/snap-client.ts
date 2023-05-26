d12d78dbe9e3c1c76ae81ab757165465a8cb076a
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2023-05-26 11:25:51 +0200

    feat: add a SnapController client

--- src/snap-client.ts
+++ src/snap-client.ts
@@ -1,16 +1,27 @@
+import { HandlerType } from '@metamask/snap-utils/*';
 import { Json } from '@metamask/utils';
 import { v4 as uuid } from 'uuid';
 
 import { Keyring, KeyringAccount, KeyringRequest } from './api';
 
-export class KeyringSnapClient implements Keyring {
-  readonly snapId: string;
+export type Sender = {
+  send<Response extends Json>({
+    method,
+    params,
+  }: {
+    method: string;
+    params?: Json[] | Record<string, Json>;
+  }): Promise<Response>;
+};
 
-  constructor(id: string) {
-    this.snapId = id;
+export class SnapRpcSender implements Sender {
+  #origin: string;
+
+  constructor(origin: string) {
+    this.#origin = origin;
   }
 
-  async #sendRequest<Response extends Json>({
+  async send<Response extends Json>({
     method,
     params,
   }: {
@@ -21,7 +32,7 @@ export class KeyringSnapClient implements Keyring {
     const response = await window.ethereum.request({
       method: 'wallet_invokeSnap',
       params: {
-        snapId: this.snapId,
+        snapId: this.#origin,
         request: {
           jsonrpc: '2.0',
           id: uuid(),
@@ -32,15 +43,65 @@ export class KeyringSnapClient implements Keyring {
     });
     return response as Response;
   }
+}
+
+export class SnapControllerSender implements Sender {
+  #snapId: string;
+
+  #origin: string;
+
+  #controller: any;
+
+  #handler: HandlerType;
+
+  constructor(
+    controller: any,
+    snapId: string,
+    origin = 'metamask',
+    handler: HandlerType = HandlerType.OnRpcRequest,
+  ) {
+    this.#controller = controller;
+    this.#snapId = snapId;
+    this.#origin = origin;
+    this.#handler = handler;
+  }
+
+  async send<Response extends Json>({
+    method,
+    params,
+  }: {
+    method: string;
+    params?: Json[] | Record<string, Json>;
+  }): Promise<Response> {
+    return await this.#controller.handleRequest({
+      snapId: this.#snapId,
+      origin: this.#origin,
+      handler: this.#handler,
+      request: {
+        jsonrpc: '2.0',
+        id: uuid(),
+        method,
+        params,
+      },
+    });
+  }
+}
+
+export class KeyringClient implements Keyring {
+  #sender: Sender;
+
+  constructor(sender: Sender) {
+    this.#sender = sender;
+  }
 
   async listAccounts(): Promise<KeyringAccount[]> {
-    return await this.#sendRequest<KeyringAccount[]>({
+    return await this.#sender.send<KeyringAccount[]>({
       method: 'keyring_listAccounts',
     });
   }
 
   async getAccount(id: string): Promise<KeyringAccount> {
-    return await this.#sendRequest<KeyringAccount>({
+    return await this.#sender.send<KeyringAccount>({
       method: 'keyring_getAccount',
       params: { id },
     });
@@ -51,62 +112,62 @@ export class KeyringSnapClient implements Keyring {
     chains: string[],
     options: Record<string, Json> | null = null,
   ): Promise<KeyringAccount> {
-    return await this.#sendRequest<KeyringAccount>({
+    return await this.#sender.send<KeyringAccount>({
       method: 'keyring_createAccount',
       params: { name, chains, options },
     });
   }
 
   async updateAccount(account: KeyringAccount): Promise<void> {
-    await this.#sendRequest<null>({
+    await this.#sender.send<null>({
       method: 'keyring_updateAccount',
       params: { account },
     });
   }
 
   async deleteAccount(id: string): Promise<void> {
-    await this.#sendRequest<null>({
+    await this.#sender.send<null>({
       method: 'keyring_deleteAccount',
       params: { id },
     });
   }
 
   async exportAccount(id: string): Promise<Record<string, Json>> {
-    return await this.#sendRequest<Record<string, Json>>({
+    return await this.#sender.send<Record<string, Json>>({
       method: 'keyring_exportAccount',
       params: { id },
     });
   }
 
   async listRequests(): Promise<KeyringRequest[]> {
-    return await this.#sendRequest<KeyringRequest[]>({
+    return await this.#sender.send<KeyringRequest[]>({
       method: 'keyring_listRequests',
     });
   }
 
   async getRequest(id: string): Promise<KeyringRequest> {
-    return await this.#sendRequest<KeyringRequest>({
+    return await this.#sender.send<KeyringRequest>({
       method: 'keyring_getRequest',
       params: { id },
     });
   }
 
   async submitRequest(request: KeyringRequest): Promise<void> {
-    await this.#sendRequest<null>({
+    await this.#sender.send<null>({
       method: 'keyring_submitRequest',
       params: request,
     });
   }
 
   async approveRequest(id: string): Promise<void> {
-    await this.#sendRequest<null>({
+    await this.#sender.send<null>({
       method: 'keyring_approveRequest',
       params: { id },
     });
   }
 
   async rejectRequest(id: string): Promise<void> {
-    await this.#sendRequest<null>({
+    await this.#sender.send<null>({
       method: 'keyring_rejectRequest',
       params: { id },
     });
