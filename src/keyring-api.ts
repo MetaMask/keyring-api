ad31412ee5a7489500e1f7aef500cf0446dc32c7
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2023-06-08 10:42:19 +0200

    chore: sort lines

--- src/keyring-api.ts
+++ src/keyring-api.ts
@@ -4,15 +4,15 @@ import { Json } from '@metamask/utils';
  * List of sign methods.
  */
 export type MethodName =
-  | 'personal_sign'
-  | 'eth_sign'
   | 'eth_sendTransaction'
+  | 'eth_sign'
   | 'eth_signTransaction'
-  | 'eth_signTypedData'
   | 'eth_signTypedData_v1'
   | 'eth_signTypedData_v2'
   | 'eth_signTypedData_v3'
-  | 'eth_signTypedData_v4';
+  | 'eth_signTypedData_v4'
+  | 'eth_signTypedData'
+  | 'personal_sign';
 
 /**
  * Account types.
