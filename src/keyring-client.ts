9c09278b4a00498e5df2e3e7bb2d5de077d7b0ba
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2023-06-15 23:10:11 +0200

    refactor: set JSON-RPC header in the `send` method

--- src/keyring-client.ts
+++ src/keyring-client.ts
@@ -5,12 +5,24 @@ import { v4 as uuid } from 'uuid';
 import {
   Keyring,
   KeyringAccount,
-  KeyringAccountStruct,
   KeyringRequest,
-  KeyringRequestStruct,
   SubmitRequestResponse,
 } from './keyring-api';
-import { InternalRequest, KeyringMethod } from './keyring-internal-api';
+import {
+  InternalRequest,
+  InternalRequestStruct,
+  KeyringMethod,
+} from './keyring-internal-api';
+
+/**
+ * Omit keys from a union type.
+ *
+ * The normal `Omit` type does not distribute over unions. So we use this
+ * workaround that applies `Omit` to each member of the union.
+ *
+ * See: https://github.com/microsoft/TypeScript/issues/31501#issuecomment-1280579305
+ */
+type OmitUnion<T, K extends keyof any> = T extends any ? Omit<T, K> : never;
 
 export type Sender = {
   send<Response extends Json>(request: InternalRequest): Promise<Response>;
@@ -19,22 +31,41 @@ export type Sender = {
 export class KeyringClient implements Keyring {
   #sender: Sender;
 
+  /**
+   * Create a new instance of `KeyringClient`.
+   *
+   * @param sender - The `Sender` instance to use to send requests to the snap.
+   */
   constructor(sender: Sender) {
     this.#sender = sender;
   }
 
-  async listAccounts(): Promise<KeyringAccount[]> {
-    return await this.#sender.send<KeyringAccount[]>({
+  /**
+   * Send a request to the snap and return the response.
+   *
+   * @param partial - Partial internal request (method and params).
+   * @returns A promise that resolves to the response to the request.
+   */
+  async #send<Response extends Json>(
+    partial: OmitUnion<InternalRequest, 'jsonrpc' | 'id'>,
+  ): Promise<Response> {
+    const request = {
       jsonrpc: '2.0',
       id: uuid(),
+      ...partial,
+    };
+    assert(request, InternalRequestStruct);
+    return await this.#sender.send<Response>(request);
+  }
+
+  async listAccounts(): Promise<KeyringAccount[]> {
+    return await this.#send<KeyringAccount[]>({
       method: KeyringMethod.ListAccounts,
     });
   }
 
   async getAccount(id: string): Promise<KeyringAccount> {
-    return await this.#sender.send<KeyringAccount>({
-      jsonrpc: '2.0',
-      id: uuid(),
+    return await this.#send<KeyringAccount>({
       method: KeyringMethod.GetAccount,
       params: { id },
     });
@@ -44,82 +75,62 @@ export class KeyringClient implements Keyring {
     name: string,
     options: Record<string, Json> | null = null,
   ): Promise<KeyringAccount> {
-    return await this.#sender.send<KeyringAccount>({
-      jsonrpc: '2.0',
-      id: uuid(),
+    return await this.#send<KeyringAccount>({
       method: KeyringMethod.CreateAccount,
       params: { name, options },
     });
   }
 
   async filterAccountChains(id: string, chains: string[]): Promise<string[]> {
-    return await this.#sender.send<string[]>({
-      jsonrpc: '2.0',
-      id: uuid(),
+    return await this.#send<string[]>({
       method: KeyringMethod.FilterAccountChains,
       params: { id, chains },
     });
   }
 
   async updateAccount(account: KeyringAccount): Promise<void> {
-    assert(account, KeyringAccountStruct);
-    await this.#sender.send<null>({
-      jsonrpc: '2.0',
-      id: uuid(),
+    await this.#send<null>({
       method: KeyringMethod.UpdateAccount,
       params: { account },
     });
   }
 
   async deleteAccount(id: string): Promise<void> {
-    await this.#sender.send<null>({
-      jsonrpc: '2.0',
-      id: uuid(),
+    await this.#send<null>({
       method: KeyringMethod.DeleteAccount,
       params: { id },
     });
   }
 
   async listRequests(): Promise<KeyringRequest[]> {
-    return await this.#sender.send<KeyringRequest[]>({
-      jsonrpc: '2.0',
-      id: uuid(),
+    return await this.#send<KeyringRequest[]>({
       method: KeyringMethod.ListRequests,
     });
   }
 
   async getRequest(id: string): Promise<KeyringRequest> {
-    return await this.#sender.send<KeyringRequest>({
-      jsonrpc: '2.0',
-      id: uuid(),
+    return await this.#send<KeyringRequest>({
       method: KeyringMethod.GetRequest,
       params: { id },
     });
   }
 
   async submitRequest(request: KeyringRequest): Promise<SubmitRequestResponse> {
-    assert(request, KeyringRequestStruct);
-    return await this.#sender.send<SubmitRequestResponse>({
-      jsonrpc: '2.0',
-      id: uuid(),
+    return await this.#send<SubmitRequestResponse>({
       method: KeyringMethod.SubmitRequest,
       params: request,
     });
   }
 
   async approveRequest(id: string): Promise<void> {
-    await this.#sender.send<null>({
-      jsonrpc: '2.0',
-      id: uuid(),
+    await this.#send<null>({
       method: KeyringMethod.ApproveRequest,
       params: { id },
     });
   }
 
   async rejectRequest(id: string): Promise<void> {
-    await this.#sender.send<null>({
-      jsonrpc: '2.0',
-      id: uuid(),
+    await this.#send<null>({
       method: KeyringMethod.RejectRequest,
       params: { id },
     });
