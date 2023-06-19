6554c6ca968a700e03d077b3fcc9159bc47d75af
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2023-06-19 22:35:18 +0200

    chore: fix sonarlint warning

--- src/utils.ts
+++ src/utils.ts
@@ -5,7 +5,7 @@ import { pattern, string } from 'superstruct';
  */
 export const UuidStruct = pattern(
   string(),
-  /^[0-9a-f]{8}-[0-9a-f]{4}-[4][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/iu,
+  /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/iu,
 );
 
 /**
