fa83da6c141b364d12cb269fd92f7df114582acc
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2023-08-23 16:11:54 +0200

    feat: add util emitter function for snaps

---
+++ src/events.ts
@@ -0,0 +1,74 @@
+import { JsonStruct } from '@metamask/utils';
+import { literal, object } from 'superstruct';
+
+import { KeyringAccountStruct } from './api';
+import { UuidStruct } from './utils';
+
+/**
+ * Supported keyring events.
+ */
+export enum KeyringEvent {
+  // Account events
+  AccountCreated = 'event:accountCreated',
+  AccountUpdated = 'event:accountUpdated',
+  AccountDeleted = 'event:accountDeleted',
+
+  // Request events
+  RequestApproved = 'event:requestApproved',
+  RequestRejected = 'event:requestRejected',
+}
+
+export const AccountCreatedEventStruct = object({
+  method: literal<string>(KeyringEvent.AccountCreated),
+  params: object({
+    /**
+     * New account object.
+     */
+    account: KeyringAccountStruct,
+  }),
+});
+
+export const AccountUpdatedEventStruct = object({
+  method: literal<string>(KeyringEvent.AccountUpdated),
+  params: object({
+    /**
+     * Updated account object.
+     */
+    account: KeyringAccountStruct,
+  }),
+});
+
+export const AccountDeletedEventStruct = object({
+  method: literal<string>(KeyringEvent.AccountDeleted),
+  params: object({
+    /**
+     * Deleted account ID.
+     */
+    id: UuidStruct,
+  }),
+});
+
+export const RequestApprovedEventStruct = object({
+  method: literal<string>(KeyringEvent.RequestApproved),
+  params: object({
+    /**
+     * Request ID.
+     */
+    id: UuidStruct,
+
+    /**
+     * Request result.
+     */
+    result: JsonStruct,
+  }),
+});
+
+export const RequestRejectedEventStruct = object({
+  method: literal<string>(KeyringEvent.RequestRejected),
+  params: object({
+    /**
+     * Request ID.
+     */
+    id: UuidStruct,
+  }),
+});
