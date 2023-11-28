ebe91f584edf67f3edb5b804d432905f3393a513
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2023-11-28 10:49:41 +0100

    feat: add `definePattern` helper function

--- src/superstruct.ts
+++ src/superstruct.ts
@@ -1,5 +1,5 @@
 import type { Infer, Context } from 'superstruct';
-import { Struct, object as stObject } from 'superstruct';
+import { Struct, define, object as stObject } from 'superstruct';
 import type {
   ObjectSchema,
   OmitBy,
@@ -100,4 +100,28 @@ export function exactOptional<Type, Schema>(
     refiner: (value, ctx) =>
       !hasOptional(ctx) || struct.refiner(value as Type, ctx),
   });
+}
+
+/**
+ * Defines a new string-struct matching a regular expression.
+ *
+ * Example:
+ *
+ * ```ts
+ * const EthAddressStruct = definePattern('EthAddress', /^0x[0-9a-f]{40}$/iu);
+ * ```
+ *
+ * @param name - Type name.
+ * @param pattern - Regular expression to match.
+ * @returns A new string-struct that matches the given pattern.
+ */
+export function definePattern(
+  name: string,
+  pattern: RegExp,
+): Struct<string, null> {
+  return define<string>(
+    name,
+    (value: unknown): boolean =>
+      typeof value === 'string' && pattern.test(value),
+  );
 }
