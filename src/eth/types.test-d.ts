fbf36568fa755c81dba5365e3bdaf84e06e3ef67
Charly Chevalier <charly.chevalier@consensys.net>
2024-04-24 09:57:15 +0200

    test: add comments for type tests + more tests eoa/erc4337

--- src/eth/types.test-d.ts
+++ src/eth/types.test-d.ts
@@ -3,11 +3,10 @@ import { expectAssignable, expectNotAssignable } from 'tsd';
 import type { EthEoaAccount, EthErc4337Account } from './types';
 import { EthAccountType, EthErc4337Method, EthMethod } from './types';
 
-// EOA
-
 const id = '606a7759-b0fb-48e4-9874-bab62ff8e7eb';
 const address = '0x000';
 
+// EOA account with no methods
 expectAssignable<EthEoaAccount>({
   type: EthAccountType.Eoa,
   id,
@@ -15,6 +14,8 @@ expectAssignable<EthEoaAccount>({
   options: {},
   methods: [],
 });
+
+// EOA account with all methods
 expectAssignable<EthEoaAccount>({
   type: EthAccountType.Eoa,
   id,
@@ -29,6 +30,8 @@ expectAssignable<EthEoaAccount>({
     `${EthMethod.SignTypedDataV4}`,
   ],
 });
+
+// EOA account with ERC-4337 methods is an error
 expectNotAssignable<EthEoaAccount>({
   type: EthAccountType.Eoa,
   id,
@@ -40,6 +43,8 @@ expectNotAssignable<EthEoaAccount>({
     `${EthErc4337Method.SignUserOperation}`,
   ],
 });
+
+// EOA account with ERC-4337 type is an error
 expectNotAssignable<EthEoaAccount>({
   type: EthAccountType.Erc4337,
   id,
@@ -52,8 +57,7 @@ expectNotAssignable<EthEoaAccount>({
   ],
 });
 
-// ERC-4337
-
+// ERC-4337 account with no methods
 expectAssignable<EthErc4337Account>({
   type: EthAccountType.Erc4337,
   id,
@@ -61,8 +65,29 @@ expectAssignable<EthErc4337Account>({
   options: {},
   methods: [],
 });
+
+// ERC-4337 account with all methods
 expectAssignable<EthErc4337Account>({
   type: EthAccountType.Erc4337,
+  id,
+  address,
+  options: {},
+  methods: [
+    `${EthMethod.PersonalSign}`,
+    `${EthMethod.Sign}`,
+    `${EthMethod.SignTransaction}`,
+    `${EthMethod.SignTypedDataV1}`,
+    `${EthMethod.SignTypedDataV3}`,
+    `${EthMethod.SignTypedDataV4}`,
+    `${EthErc4337Method.PrepareUserOperation}`,
+    `${EthErc4337Method.PatchUserOperation}`,
+    `${EthErc4337Method.SignUserOperation}`,
+  ],
+});
+
+// ERC-4337 account with only user-ops methods
+expectNotAssignable<EthErc4337Account>({
+  type: EthAccountType.Eoa,
   id,
   address,
   options: {},
@@ -72,6 +97,8 @@ expectAssignable<EthErc4337Account>({
     `${EthErc4337Method.SignUserOperation}`,
   ],
 });
+
+// ERC-4337 account with EOA type is an error
 expectNotAssignable<EthErc4337Account>({
   type: EthAccountType.Eoa,
   id,
