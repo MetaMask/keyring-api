042caf7eba97e53bdbf0de72dc1f9770b3ba92ce
Charly Chevalier <charly.chevalier@consensys.net>
2024-04-25 19:13:07 +0200

    feat: add bip122:p2wpkh account support (#294)
    * feat: add bip122:p2wpkh account support
    
    * refactor: add and use asInternalAccountStruct for internal accounts

---
+++ src/btc/types.ts
@@ -0,0 +1,51 @@
+import { bech32 } from 'bech32';
+import type { Infer } from 'superstruct';
+import { object, string, array, enums, literal, refine } from 'superstruct';
+
+import { BaseAccount } from '../base-types';
+
+export const BtcP2wpkhAddressStruct = refine(
+  string(),
+  'BtcP2wpkhAddressStruct',
+  (address: string) => {
+    try {
+      bech32.decode(address);
+    } catch (error) {
+      return new Error(
+        `Could not decode P2WPKH address: ${(error as Error).message}`,
+      );
+    }
+    return true;
+  },
+);
+
+/**
+ * Supported Bitcoin methods.
+ */
+export enum BtcMethod {
+  // General transaction methods
+  SendTransaction = 'btc_sendTransaction',
+}
+
+/**
+ * Supported Bitcoin account types.
+ */
+export enum BtcAccountType {
+  P2wpkh = 'bip122:p2wpkh',
+}
+
+export const BtcP2wpkhAccountStruct = object({
+  ...BaseAccount,
+
+  /**
+   * Account type.
+   */
+  type: literal(`${BtcAccountType.P2wpkh}`),
+
+  /**
+   * Account supported methods.
+   */
+  methods: array(enums([`${BtcMethod.SendTransaction}`])),
+});
+
+export type BtcP2wpkhAccount = Infer<typeof BtcP2wpkhAccountStruct>;
--- src/internal/types.ts
+++ src/internal/types.ts
@@ -2,6 +2,7 @@ import type { Infer, Struct } from 'superstruct';
 import { boolean, string, number, define, mask, validate } from 'superstruct';
 
 import { BaseKeyringAccountStruct } from '../api';
+import { BtcP2wpkhAccountStruct, BtcAccountType } from '../btc/types';
 import {
   EthEoaAccountStruct,
   EthErc4337AccountStruct,
@@ -27,28 +28,51 @@ export const InternalAccountMetadataStruct = object({
   }),
 });
 
-export const InternalEthEoaAccountStruct = object({
-  ...EthEoaAccountStruct.schema,
-  ...InternalAccountMetadataStruct.schema,
-});
+/**
+ * Creates an `InternalAccount` from an existing account `superstruct` object.
+ *
+ * @param accountStruct - An account `superstruct` object.
+ * @returns The `InternalAccount` assocaited to `accountStruct`.
+ */
+function asInternalAccountStruct<Account, AccountSchema>(
+  accountStruct: Struct<Account, AccountSchema>,
+) {
+  return object({
+    ...accountStruct.schema,
+    ...InternalAccountMetadataStruct.schema,
+  });
+}
+
+export const InternalEthEoaAccountStruct =
+  asInternalAccountStruct(EthEoaAccountStruct);
+
+export const InternalEthErc4337AccountStruct = asInternalAccountStruct(
+  EthErc4337AccountStruct,
+);
+
+export const InternalBtcP2wpkhAccountStruct = asInternalAccountStruct(
+  BtcP2wpkhAccountStruct,
+);
 
 export type InternalEthEoaAccount = Infer<typeof InternalEthEoaAccountStruct>;
-
-export const InternalEthErc4337AccountStruct = object({
-  ...EthErc4337AccountStruct.schema,
-  ...InternalAccountMetadataStruct.schema,
-});
 
 export type InternalEthErc4337Account = Infer<
   typeof InternalEthErc4337AccountStruct
 >;
 
+export type InternalBtcP2wpkhAccount = Infer<
+  typeof InternalBtcP2wpkhAccountStruct
+>;
+
 export const InternalAccountStructs: Record<
   string,
-  Struct<InternalEthEoaAccount> | Struct<InternalEthErc4337Account>
+  | Struct<InternalEthEoaAccount>
+  | Struct<InternalEthErc4337Account>
+  | Struct<InternalBtcP2wpkhAccount>
 > = {
   [`${EthAccountType.Eoa}`]: InternalEthEoaAccountStruct,
   [`${EthAccountType.Erc4337}`]: InternalEthErc4337AccountStruct,
+  [`${BtcAccountType.P2wpkh}`]: InternalBtcP2wpkhAccountStruct,
 };
 
 export const InternalAccountStruct = define(
