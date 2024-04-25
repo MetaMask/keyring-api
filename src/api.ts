0e2028562b38af88a0668025fe7d932eb30d5209
Charly Chevalier <charly.chevalier@consensys.net>
2024-04-25 10:50:36 +0200

    chore: typo messager -> message
    Co-authored-by: Daniel Rocha <daniel.rocha@consensys.net>

--- src/api.ts
+++ src/api.ts
@@ -54,7 +54,7 @@ export const KeyringAccountStruct = define<KeyringAccounts>(
   // We do use a custom `define` for this type to avoid having to use a `union` since error
   // messages are a bit confusing.
   // Doing manual validation allows us to use the "concrete" type of each supported acounts giving
-  // use a much nicer messager from `superstruct`.
+  // use a much nicer message from `superstruct`.
   'KeyringAccount',
   (value: unknown) => {
     // This will also raise if `value` does not match any of the supported account types!
