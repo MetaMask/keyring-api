3c480678c6cbf7aad6a051f4108c7899f2a1f0ee
Monte Lai <monte.lai@consensys.net>
2024-04-30 22:48:44 +0800

    fix: update logic to pass in account type rather than account object

--- src/helpers.test.ts
+++ src/helpers.test.ts
@@ -1,40 +1,17 @@
-import { BtcAccountType, BtcMethod } from './btc';
-import { EthAccountType, EthErc4337Method, EthMethod } from './eth';
-import { isEvmAccount } from './helpers';
-import type { InternalAccountType } from './internal';
+import { BtcAccountType } from './btc';
+import { EthAccountType } from './eth';
+import { isEvmAccountType } from './helpers';
 
-const createMockAccount = (type: InternalAccountType) => {
-  const accountMethods = {
-    [BtcAccountType.P2wpkh]: [`${BtcMethod.SendMany}`],
-    [EthAccountType.Eoa]: [`${EthMethod.PersonalSign}`],
-    [EthAccountType.Erc4337]: [`${EthErc4337Method.SignUserOperation}`],
-  };
-  return {
-    id: '606a7759-b0fb-48e4-9874-bab62ff8e7eb',
-    address: '0x000',
-    options: {},
-    methods: accountMethods[type],
-    type,
-    metadata: {
-      keyring: {
-        type: 'mock-keyring',
-      },
-      name: 'Account 1',
-      importTime: 1713153716,
-    },
-  };
-};
-
-describe('isEVMAccount', () => {
+describe('isEVMAccountType', () => {
   it.each([
-    [createMockAccount(EthAccountType.Eoa), true],
-    [createMockAccount(EthAccountType.Erc4337), true],
-    [createMockAccount(BtcAccountType.P2wpkh), false],
+    [EthAccountType.Eoa, true],
+    [EthAccountType.Erc4337, true],
+    [BtcAccountType.P2wpkh, false],
     [{}, false],
     [null, false],
-    [{ type: EthAccountType.Eoa }, false],
-  ])('returns true for evm account', (account, result) => {
+    ['bitcoin', false],
+  ])('%s should return %s', (account, result) => {
     // @ts-expect-error for error cases
-    expect(isEvmAccount(account)).toBe(result);
+    expect(isEvmAccountType(account)).toBe(result);
   });
 });
