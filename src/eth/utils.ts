2172860436653039c32b5df27ccdce497d68b98d
Monte Lai <monte.lai@consensys.net>
2024-05-02 21:31:43 +0800

    fix: update jsdoc

--- src/eth/utils.ts
+++ src/eth/utils.ts
@@ -2,9 +2,9 @@ import type { InternalAccountType } from '../internal';
 import { EthAccountType } from './types';
 
 /**
- * Checks if the given internal account is of the EVM account type.
+ * Checks if the given type is an EVM account type.
  * @param type - The type to check.
- * @returns Returns true if the internal account has an EVM account type, false otherwise.
+ * @returns Returns true if the type is an EVM account type, false otherwise.
  */
 export function isEvmAccountType(type: InternalAccountType): boolean {
   return type === EthAccountType.Eoa || type === EthAccountType.Erc4337;
