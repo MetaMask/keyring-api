872c6051f654fcdefdaa2e84b3235c5826e58795
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2024-05-22 21:43:27 +0200

    fix: don't use internal types in the public API

--- src/eth/utils.ts
+++ src/eth/utils.ts
@@ -1,11 +1,12 @@
+import type { KeyringAccount } from '../api';
 import { EthAccountType } from '../api';
-import type { InternalAccountType } from '../internal';
 
 /**
  * Checks if the given type is an EVM account type.
+ *
  * @param type - The type to check.
  * @returns Returns true if the type is an EVM account type, false otherwise.
  */
-export function isEvmAccountType(type: InternalAccountType | string): boolean {
+export function isEvmAccountType(type: KeyringAccount['type']): boolean {
   return type === EthAccountType.Eoa || type === EthAccountType.Erc4337;
 }
